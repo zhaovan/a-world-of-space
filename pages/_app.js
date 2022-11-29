16e9639491b554240cae622edcce79c348a626cf
Ivan Zhao <ivanzhao8@gmail.com>
2022-11-28 16:59:05 -0800

    bruh

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import localFont from "@next/font/local";
+
+// Font files can be colocated inside of `pages`
+const myFont = localFont({
+  src: [
+    {
+      path: "./fonts/PixeloidSans.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/PixeloidSansBold.ttf",
+      weight: "bold",
+      style: "normal",
+    },
+  ],
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={myFont.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ script/script.js
@@ -0,0 +1,170 @@
+const text = [
+  { display: "recommended: having music on low", button: "get started" },
+  { display: "hey", button: "hey?" },
+  { display: "oh... you must be new here...", button: "I guess?" },
+  {
+    display:
+      "that's cool. welcome to a tiny infinite speck in the black void of the internet",
+    button: "ha ha",
+  },
+  {
+    display:
+      "no i'm serious... it's just you and me here... we've got all the time in the world",
+    button: "what does that actually mean?",
+  },
+  {
+    display: "mmmm well i mean time passes differently here.",
+    button: "that doesn't explain anything",
+  },
+  { display: "ever heard of twitter?", button: "no?" },
+  {
+    display:
+      "it's this weird amalgamation of the best and worst things in the world... it's like your college dorm but half the people have slept together and the half that haven't are desperately throwing themselves at those who have.",
+    button: "gross",
+  },
+  {
+    display:
+      "exactly!!  but when you're on it... it feels like time both slows down and passes extremely quickly. it doesn't make sense",
+    button: "...I guess not",
+  },
+  {
+    display:
+      "anyways, while I have you here, why don't you cozy up and make yourself comfortable. coffee?",
+    button: "got any espresso?",
+  },
+  {
+    display:
+      "ah... we've got a pretentious one on our hands. lemme dig around in the back to see if we've got a machine",
+    button: "thank you",
+  },
+  { display: "...", button: "..." },
+  { display: "...", button: "..." },
+  { display: "...", button: "..." },
+  { display: "...!", button: "...?" },
+  {
+    display: "you're in luck kid... I found one in the back",
+    button: "can you make it a double? didn't sleep well last night",
+  },
+  { display: "don't push it...", button: "haha sorry" },
+  { display: "*coffee machine whirs*", button: "..." },
+  {
+    display: "*a faint grunting sound can be heard in the back*",
+    button: "...?",
+  },
+  {
+    display: "...",
+    button: "*it sound's like he's struggling... maybe I should help?*",
+  },
+  {
+    display: "all done! here ya go... one double shot, on the house!",
+    button: "thanks. I didn't have any money to begin with though",
+  },
+  { display: "good thing it's on us then *winks*", button: "alright..." },
+  {
+    display: "mmmm all cozy now? want to hear a story?",
+    button: "sure? what else do I have to do?",
+  },
+  { display: "that's the spirit!", button: "sigh..." },
+  {
+    display:
+      "a boy and a father are out on a shopping trip. the boy wants to stop by at the ice cream parlor. the father, being the dutiful dad, obliges",
+    button: "...",
+  },
+  {
+    display:
+      "at the shop, the boy looks around at all the flavors, indulging in the choices. in an almost drunken stupor, overwhelmed, the boy asks the father to pick for him.",
+    button: "...",
+  },
+  {
+    display:
+      "knowing his favorites, the father picks a double scoop of cookie dough. the boy claps giddily, and grabs the overflowing waffle cone lined with little dough nuggets",
+    button: "...",
+  },
+  {
+    display:
+      "he is happy now... what more could he want in the world? god bestowed upon him the greatest gift",
+    button: "...",
+  },
+  {
+    display:
+      "twenty years pass, and the boy is a boy no longer. in weathered hands are a fresh bouquet of flowers",
+    button: "...",
+  },
+  {
+    display:
+      "the father is now in the infirmary, a heart too full placed him there",
+    button: "...",
+  },
+  {
+    display: "the boy is ushered into the room, a faint beeping can be heard",
+    button: "...",
+  },
+  {
+    display:
+      "the doctor looks at him with a heavy heart, and hands him the papers. he can choose to pull the plug whenever, he is told, but there is no rush",
+    button: "...",
+  },
+  {
+    display:
+      "only the slow, gnawing, pull of death; vertigo, from the passage of time.",
+    button: "...",
+  },
+  {
+    display:
+      "the boy is conflicted... the boy had been coming here for months now... why now?",
+    button: "...",
+  },
+  {
+    display:
+      "the doctor said that the father was in terrible pain, that to keep going would be more damaging than good",
+    button: "...",
+  },
+  {
+    display:
+      "the old bouquet of flowers sits in a glass vase nearby. the once buttery daffodils, creamy daisies, and chocolately tulips dribble over the sides of the bowl",
+    button: "...",
+  },
+  {
+    display:
+      "the boy wants some time to think, to ponder his options. the doctor says of course and gently retreats from the room",
+    button: "...",
+  },
+  {
+    display:
+      "the boy wonders how he could ever do such a thing. and to have a choice in the death of his father",
+    button: "...",
+  },
+  {
+    display:
+      "he thinks back to the years that they had spent together. he would never give that up",
+    button: "...",
+  },
+  { display: "but ultimately...", button: "..." },
+  { display: "...", button: "..." },
+  {
+    display: "...",
+    button: "pull the plug/let him go slowly",
+    choice: true,
+  },
+  { display: "you chose to", button: "..." },
+  { display: "i guess that's fair", button: "..." },
+  {
+    display:
+      "we're all simply an ocean of thoughts, with a tiny leak, dripping to the ends of existence",
+    button: "...",
+  },
+  { display: "and it all comes to a halt", button: "..." },
+  {
+    display:
+      "i might've thought it would have ended different... after all I did for you",
+    button: "...",
+  },
+  {
+    display: "but I guess not",
+    button: "...",
+  },
+  { display: "thanks for this brief moment together", button: "..." },
+  { display: "goodbye", button: "..." },
+];
+
+export default text;
